9701f190e99b34fed7e553e546b525e75786365a
Tarcio <tarcio@gmail.com>
2014-03-22 22:10:15 +1100

    Some changes around jshint

--- test/nodecreateSpec.js
+++ test/nodecreateSpec.js
@@ -2,13 +2,13 @@ var app = require('../lib/nodecreate');
 var readline = require('readline');
 var sinon = require('sinon');
 
-describe('app', function() {
+describe('app', function () {
 
-  describe('#run', function() {
+  describe('#run', function () {
 
     var rlSpy = sinon.spy(readline, 'createInterface');
 
-    it('should prompt user for 6 questions', function() {
+    it('should prompt user for 6 questions', function () {
       app.run();
     });
 
